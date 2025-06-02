cd998845b6ac16758445ebf6397f5ecaadadd70b
Eslam Bashry <eslamhussin600@gmail.com>
2025-06-03 01:38:08 +0300

    inventory apis and update schema 🐈

--- src/modules/inventory/inventory.routes.js
+++ src/modules/inventory/inventory.routes.js
@@ -6,5 +6,9 @@ const inventoryRouter = Router()
 
 
 inventoryRouter.get('/all', inventoryRoutertCont.getInventory);
+inventoryRouter.get('/:id', inventoryRoutertCont.getInventoryById);
+inventoryRouter.post('/create', inventoryRoutertCont.createInventory);
+inventoryRouter.put('/:id', inventoryRoutertCont.updateInventory);
+inventoryRouter.delete('/:id', inventoryRoutertCont.deleteInventory);
 
 export default inventoryRouter
